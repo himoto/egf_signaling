0f74831423b03553bc3ec31031257c518cb66ef8
himoto <himoto@protein.osaka-u.ac.jp>
2019-08-01 17:56:59 +0900

    Update plotFunc.m

--- plotFunc.m
+++ plotFunc.m
@@ -1,11 +1,11 @@
-figure('Renderer', 'painters', 'Position', [0 0 800 600]);
+figure('Renderer', 'painters', 'Position', [0 0 700 600]);
 
 subplot(2,2,1);
 hold on;
 plot(t,totalShc(:,1),'r-');
-plot(t,totalShc(:,2),'r--');
+plot(t,totalShc(:,2),'m-');
 plot(t,totalGrb2(:,1),'b-');
-plot(t,totalGrb2(:,2),'b--');
+plot(t,totalGrb2(:,2),'c-');
 set(gca, 'Xtick', [0 30 60 90 120],...
     'Xticklabel', [0 30 60 90 120]);
 ylim([0 150]);
@@ -18,9 +18,9 @@ box on;
 subplot(2,2,2);
 hold on;
 plot(t,RSh(:,1),'r-');
-plot(t,RSh(:,2),'r--');
+plot(t,RSh(:,2),'m-');
 plot(t,RGrb2(:,1),'b-');
-plot(t,RGrb2(:,2),'b--');
+plot(t,RGrb2(:,2),'c-');
 set(gca, 'Xtick', [0 30 60 90 120],...
     'Xticklabel', [0 30 60 90 120]);
 ylim([0 150]);
@@ -34,19 +34,22 @@ box on;
 subplot(2,2,3);
 hold on;
 yyaxis left;
-plot(t,totalSOS(:,1),'-');
-plot(t,totalSOS(:,2),'--');
+plot(t,totalSOS(:,1),'b-');
+plot(t,totalSOS(:,2),'c-');
 set(gca, 'Xtick', [0 30 60 90 120],...
     'Xticklabel', [0 30 60 90 120]);
 ylim([0 150]);
 xlim([0 120]);
 xlabel("TIME (s)");
 ylim([0 8]);
+set(gca, 'Ytick', [0 2 4 6 8],...
+    'Yticklabel', [0 2 4 6 8]);
+
 ylabel("SOS bound to EGFR (nM)");
 
 yyaxis right;
-plot(t,ShGS(:,1),'-');
-plot(t,ShGS(:,2),'--');
+plot(t,ShGS(:,1),'r-');
+plot(t,ShGS(:,2),'m-');
 
 xlim([0 120]);
 ylim([0 30]);
@@ -67,7 +70,7 @@ xlabel("TIME (s)");
 ylabel("Total Phosphorylated PLCγ (nM)");
 
 yyaxis right;
-plot(t,PLCgamma(:,3),'--');
+plot(t,PLCgamma(:,3),'b--');
 ylim([0 105]);
 set(gca, 'Ytick', [0 30 60 90],...
     'Yticklabel', [0 30 60 90]);
